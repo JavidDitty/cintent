c9eb17e7606f10936b1210839aba8a61f43b70fb
JavidDitty <javidditty@gmail.com>
2025-11-13 15:36:02 -0500

    Parse transition graph from speedscope

--- src/cintent/speedscope.py
+++ src/cintent/speedscope.py
@@ -4,6 +4,7 @@ import json
 import os
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 from pandas import DataFrame
 
@@ -21,19 +22,20 @@ class Speedscope:
             self.functions = pd.read_csv(self.functions_file)
 
         # Parse Speedscope representations
-        self.sandwich = self.sandwich()
+        self.sandwich = self.__sandwich()
+        self.graph = self.__graph()
     
-    def sandwich(self) -> dict:
+    def __sandwich(self) -> DataFrame:
         """Aggregate the weights for each frame"""
         frames = self.base['shared']['frames']
         for profile in self.base['profiles']:
             for sample, weight in zip(profile['samples'], profile['weights']):
                 for frame in sample:
-                    frame_idx = frame - 1
+                    frame_idx = frame
                     if 'weight' not in frames[frame_idx]:
                         frames[frame_idx]['weight'] = 0
                     frames[frame_idx]['weight'] += weight
-                    if any(item not in frames[frame_idx] for item in ('fq_name', 'header', 'relpath')):
+                    if self.functions_file and any(item not in frames[frame_idx] for item in ('fq_name', 'header', 'relpath')):
                         functions = self.functions
                         functions = functions[functions['name'] == frames[frame_idx]['name']]
                         functions = functions[functions['line'] == frames[frame_idx]['line']]
@@ -48,22 +50,64 @@ class Speedscope:
                             frames[frame_idx]['fq_name'] = None
                             frames[frame_idx]['header'] = None
                             frames[frame_idx]['relpath'] = None
-        return frames
+                        frames[frame_idx]['frame_idx'] = frame_idx
+        frames_df = DataFrame(frames)[['frame_idx','name','fq_name','header','file','relpath','line','col','weight']]
+        return frames_df
     
-    def to_csv(self, out_path: str) -> None:
-        """Dump sandwich representation to a CSV"""
-        sandwich_df = DataFrame(self.sandwich)
-        sandwich_df.to_csv(out_path, index=None, quoting=csv.QUOTE_ALL)
+    def __graph(self) -> DataFrame:
+        """Create a transition graph that includes all the frames"""
+        # Get the frame stacks and the unique frames
+        samples, frames = [], set()
+        for profile in self.base['profiles']:
+            for sample in profile['samples']:
+                frames.update(sample)
+            samples += profile['samples']
+        frames = sorted(frames)
+        num_of_frames = len(frames)
+        frame_to_index = {frame: i for i, frame in enumerate(frames)}
+        index_to_name = {row.frame_idx: row.fq_name for row in self.sandwich.itertuples()}
+
+        # Create the transition count matrix
+        transition_counts = np.zeros((num_of_frames, num_of_frames), dtype=int)
+        for sample in samples:
+            for i in range(len(sample)-1):
+                curr = frame_to_index[sample[i]]
+                next = frame_to_index[sample[i+1]]
+                transition_counts[curr, next] += 1
+
+        # Normalize the counts to probabilities
+        transition_matrix = np.zeros((num_of_frames, num_of_frames), dtype=float)
+        for i in range(num_of_frames):
+            row_sum = transition_counts[i, :].sum()
+            if row_sum > 0:
+                transition_matrix[i, :] = transition_counts[i, :] / row_sum
+        
+        # Convert transition counts and matrix to a dataframe
+        transition_df = []
+        indicies = list(range(len(transition_counts)))
+        for i, c_row, p_row in zip(indicies, transition_counts, transition_matrix):
+            for j, c_cell, p_cell in zip(indicies, c_row, p_row):
+                if index_to_name[i] and index_to_name[j] and c_cell > 0:
+                    transition_df.append({
+                        'src': index_to_name[i], 
+                        'dest': index_to_name[j], 
+                        'count': c_cell, 
+                        'probability': p_cell,
+                    })
+        transition_df = DataFrame(transition_df).sort_values(by='count', ascending=False)
+        return transition_df
 
 
 def to_csv(speedscope_file: str, out_dir: str, functions_file: str | None = None) -> None:
     # Parse a speedscope file
     speedscope = Speedscope(speedscope_file=speedscope_file, functions_file=functions_file)
 
-    # Dump the speedscope parse to a file
-    filename = f'{Path(speedscope_file).stem}.speedscope.csv'
-    out_path = os.path.join(out_dir, filename)
-    speedscope.to_csv(out_path)
+    # Dump the speedscope representation(s)
+    filename = Path(speedscope_file).stem
+    sandwich_path = os.path.join(out_dir, f'{filename}.sandwich.csv')
+    graph_path = os.path.join(out_dir, f'{filename}.graph.csv')
+    speedscope.sandwich.to_csv(sandwich_path, index=None, quoting=csv.QUOTE_ALL)
+    speedscope.graph.to_csv(graph_path, index=None, quoting=csv.QUOTE_ALL)
 
 
 def parse_args() -> Namespace:
